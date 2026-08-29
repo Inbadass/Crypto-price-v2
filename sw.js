f29f8da8518899e2eae4df30416fcb125cd19e9e
Inbadass <inbadass3.14@gmail.com>
2026-08-29 15:27:03 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener("install", function() {
+console.log("Service Worker installed.");
+});
+self.addEventListener("activate", function() {
+console.log("Service Worker activated.");
+});
